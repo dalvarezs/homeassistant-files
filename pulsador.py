c1f016005202e473248267ec43dc5ef10d71fdc8
dalvarezs <dalvarezs@users.noreply.github.com>
2019-10-15 21:41:38 +0200

    Added button to turn on the boiler

--- pulsador.py
+++ pulsador.py
@@ -22,22 +22,38 @@ logger.addHandler(handler)
 
 # https://www.prometec.net/condicionales-y-pulsadores/
 
-# 1 pin al GPIO 24 (PIN 18) y otro a GND por Pull UP
-
+#ON: 1 pin al GPIO 24 (PIN 18) y otro a GND por Pull UP
+#OFF: 1 pin al GPIO 25 y otro a GND por Pull UP
 GPIO.setmode(GPIO.BCM) # Nomemclatura GPIO, en lugar de numero de PIN
 GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(25, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 logger.info('Booting')
 
 
 while True:
-    input_state = GPIO.input(24)
-    if input_state == False:
-        logger.info('Boton pulsado')
+    input_stateON = GPIO.input(24)
+    if input_stateON == False:
+        print('Boton ON pulsado')
+        logger.info('Boton ON pulsado')
         time.sleep(.5)
-        input_state = GPIO.input(24)
-        if input_state == False:
-            logger.info('Boton pulsado2')
-            cmd_line = "/usr/bin/mosquitto_pub -h IP -u USER -P PASSWORD -t '/house/switchCalderaCmd/' -m '1'"
+        input_stateON = GPIO.input(24)
+        if input_stateON == False:
+            print('Boton ON pulsado2')
+            logger.info('Boton ON pulsado2')
+            cmd_line = "/usr/bin/mosquitto_pub -h 192.168.33.131 -u davidmqtt -P haDamqtt3 -t '/house/switchCalderaCmd/' -m '1'"
+            args = shlex.split(cmd_line)
+            subprocess.Popen(args)
+            time.sleep(1)
+    input_stateOFF = GPIO.input(25)
+    if input_stateOFF == False:
+        print('Boton OFF pulsado')
+        logger.info('Boton OFF pulsado')
+        time.sleep(.5)
+        input_stateOFF = GPIO.input(25)
+        if input_stateOFF == False:
+            print('Boton OFF pulsado2')
+            logger.info('Boton OFF pulsado2')
+            cmd_line = "/usr/bin/mosquitto_pub -h 192.168.33.131 -u davidmqtt -P haDamqtt3 -t '/house/switchCalderaCmd/' -m '0'"
             args = shlex.split(cmd_line)
             subprocess.Popen(args)
             time.sleep(1)
