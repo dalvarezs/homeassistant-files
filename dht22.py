5a71433df69c34ae162e6681c26890b108ea2337
dalvarezs <dalvarezs@users.noreply.github.com>
2020-12-02 23:10:05 +0100

    Update dht22.py
    2 decimals sensor values

--- dht22.py
+++ dht22.py
@@ -15,8 +15,8 @@ while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
     if humidity is not None and temperature is not None:
-#        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-        temp='{"temperature": "'+str(temperature)+'","humidity": "'+str(humidity)+'"}'
+        temp='{"temperature": "'+'%.2f' %temperature+'","humidity": "'+'%.2f' %humidity+'"}'
+        print(temp)
         publish.single("<topic>",
             payload=temp,
             hostname="<IP>",
