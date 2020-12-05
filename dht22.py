e29711883779b6cfb90522dc9e5a1c3abaa1fb3c
dalvarezs <dalvarezs@users.noreply.github.com>
2020-12-05 13:48:22 +0100

    Update dht22.py
    added logging capabilities

--- dht22.py
+++ dht22.py
@@ -2,6 +2,21 @@ import Adafruit_DHT
 import paho.mqtt.publish as publish
 import paho.mqtt.client as mqtt
 import time
+import logging
+
+logger = logging.getLogger("dht22")
+logger.setLevel(logging.INFO)
+
+# create a file handler
+handler = logging.FileHandler('iot.log')
+handler.setLevel(logging.INFO)
+
+# create a logging format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(handler)
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
@@ -11,12 +26,14 @@ auth = {
   'password':"<mqtt_passwd>"
 }
 
+logger.info('Booting')
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
     if humidity is not None and temperature is not None:
         temp='{"temperature": "'+'%.2f' %temperature+'","humidity": "'+'%.2f' %humidity+'"}'
         print(temp)
+        logger.info(temp)
         publish.single("<topic>",
             payload=temp,
             hostname="<IP>",
@@ -25,5 +42,6 @@ while True:
             port=1883)
     else:
 #        print("Failed to retrieve data from DHT sensor")
+         logger.info('Failed to retrieve data from humidity sensor')
     
     time.sleep(600)
