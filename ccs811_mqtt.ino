1a0223d9382f3f161d8a7047d1db091ca038fc87
dalvarezs <dalvarezs@users.noreply.github.com>
2020-12-07 20:05:40 +0100

    Create ccs811_mqtt.ino

---
+++ ccs811_mqtt.ino
@@ -0,0 +1,171 @@
+/*
+  Based on ccs811basic.ino - Demo sketch printing results of the CCS811 digital gas sensor for monitoring indoor air quality from ams. Created by Maarten Pennings 2017 Dec 11
+
+  @dalvarez_s 2020/12/07
+  
+*//
+/
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <Wire.h>    // I2C library
+#include "ccs811.h"  // CCS811 library
+
+#define MQTT_VERSION MQTT_VERSION_3_1_1
+
+
+// Wifi: SSID and password
+const char* WIFI_SSID = "[redacted]";
+const char* WIFI_PASSWORD = "[redacted]";
+
+// MQTT: ID, server IP, port, username and password
+const PROGMEM char* MQTT_CLIENT_ID = "[redacted]";
+const PROGMEM char* MQTT_SERVER_IP = "[redacted]";
+const PROGMEM uint16_t MQTT_SERVER_PORT = [redacted];
+const PROGMEM char* MQTT_USER = "[redacted]";
+const PROGMEM char* MQTT_PASSWORD = "[redacted]";
+
+// MQTT: topic
+const PROGMEM char* MQTT_SENSOR_TOPIC = "house/salon/ccs811";
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+// function called to publish the co2 values
+void publishData(float p_co2) {
+  // create a JSON object
+  const size_t CAPACITY = JSON_OBJECT_SIZE(128); //MQTT_MAX_PACKET_SIZE
+  StaticJsonDocument<CAPACITY> jsonBuffer;
+  JsonObject root = jsonBuffer.to<JsonObject>();
+  
+  // INFO: the data must be converted into a string; a problem occurs when using floats...
+  root["co2_ppm"] = (String)p_co2;
+  serializeJsonPretty(root, Serial);
+  Serial.println("");
+  /*
+     {
+        "co2_ppm": "450"
+     }
+  */
+  char data[128]; //MQTT_MAX_PACKET_SIZE
+  serializeJson(root, data, measureJson(root) + 1);
+  client.publish(MQTT_SENSOR_TOPIC, data, true);
+  yield();
+}
+
+// function called when a MQTT message arrived
+void callback(char* p_topic, byte* p_payload, unsigned int p_length) {
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.println("INFO: Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(MQTT_CLIENT_ID, MQTT_USER, MQTT_PASSWORD)) {
+      Serial.println("INFO: connected");
+    } else {
+      Serial.print("ERROR: failed, rc=");
+      Serial.print(client.state());
+      Serial.println("DEBUG: try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+// Wiring for ESP8266 NodeMCU boards: VDD to 3V3, GND to GND, SDA to D2, SCL to D1, nWAKE to D3 (or GND)
+CCS811 ccs811(D3); // nWAKE on D3
+
+// Wiring for Nano: VDD to 3v3, GND to GND, SDA to A4, SCL to A5, nWAKE to 13
+//CCS811 ccs811(13); 
+
+// nWAKE not controlled via Arduino host, so connect CCS811.nWAKE to GND
+//CCS811 ccs811; 
+
+// Wiring for ESP32 NodeMCU boards: VDD to 3V3, GND to GND, SDA to 21, SCL to 22, nWAKE to D3 (or GND)
+//CCS811 ccs811(23); // nWAKE on 23
+
+
+void setup() {
+  // Enable serial
+  Serial.begin(115200);
+  Serial.println("");
+  Serial.println("setup: Starting CCS811 basic demo");
+  Serial.print("setup: ccs811 lib  version: "); Serial.println(CCS811_VERSION);
+
+  // Enable I2C
+  Wire.begin(); 
+  
+  // Enable CCS811
+  ccs811.set_i2cdelay(50); // Needed for ESP8266 because it doesn't handle I2C clock stretch correctly
+  bool ok= ccs811.begin();
+  if( !ok ) Serial.println("setup: CCS811 begin FAILED");
+
+  // Print CCS811 versions
+  Serial.print("setup: hardware    version: "); Serial.println(ccs811.hardware_version(),HEX);
+  Serial.print("setup: bootloader  version: "); Serial.println(ccs811.bootloader_version(),HEX);
+  Serial.print("setup: application version: "); Serial.println(ccs811.application_version(),HEX);
+  
+  // Start measuring
+  ok= ccs811.start(CCS811_MODE_1SEC); //mode 1
+  if( !ok ) Serial.println("setup: CCS811 start FAILED");
+
+  // Wait 20 minutes for accurate readings (Run-In)
+  delay(1200000); 
+
+  // init the WiFi connection
+  Serial.println();
+  Serial.println();
+  Serial.print("INFO: Connecting to ");
+  WiFi.mode(WIFI_STA);
+  Serial.println(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("INFO: WiFi connected");
+  Serial.println("INFO: IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // init the MQTT connection
+  client.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);
+  client.setCallback(callback); 
+}
+
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  
+  // Read co2 (ppm)
+  uint16_t eco2, etvoc, errstat, raw;
+  ccs811.read(&eco2,&etvoc,&errstat,&raw); 
+
+  // Print measurement results based on status
+  if( errstat==CCS811_ERRSTAT_OK ) { 
+    Serial.print("CCS811: ");
+    Serial.print("eco2=");  Serial.print(eco2);     Serial.print(" ppm  ");
+    publishData(eco2);
+    Serial.print("etvoc="); Serial.print(etvoc);    Serial.print(" ppb  ");
+    //Serial.print("raw6=");  Serial.print(raw/1024); Serial.print(" uA  "); 
+    //Serial.print("raw10="); Serial.print(raw%1024); Serial.print(" ADC  ");
+    //Serial.print("R="); Serial.print((1650*1000L/1023)*(raw%1024)/(raw/1024)); Serial.print(" ohm");
+    Serial.println();
+  } else if( errstat==CCS811_ERRSTAT_OK_NODATA ) {
+    Serial.println("CCS811: waiting for (new) data");
+  } else if( errstat & CCS811_ERRSTAT_I2CFAIL ) { 
+    Serial.println("CCS811: I2C error");
+  } else {
+    Serial.print("CCS811: errstat="); Serial.print(errstat,HEX); 
+    Serial.print("="); Serial.println( ccs811.errstat_str(errstat) ); 
+  }
+  
+  // Wait
+  delay(5000); 
+}
